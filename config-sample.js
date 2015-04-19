b5c5d73f5c8cb42e55014706850deaf6953dd450
Colin Loretz <colin@numerousapp.com>
2015-04-18 20:41:08 -0700

    added config sample file

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+/* 
+	GETTING STARTED
+	Create a new version of this file called config.js and add your API keys below
+*/
+
+module.exports = {
+	pinoccioKey : "YOUR_PINOCCIO_KEY"
+	wundergroundKey : "YOUR_WUNDERGROUND_KEY"
+}
